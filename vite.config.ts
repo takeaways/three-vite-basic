2944772346f09c220b4e21764c1b8f5624ec2c8b
takeaways <wkdrjsdlf2@gmail.com>
2022-05-22 02:25:23 +0900

    init project

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+const genAlias = (aliases: Array<string>) =>
+  aliases.map((alias) => ({
+    find: alias,
+    replacement: resolve(__dirname, "src", alias),
+  }));
+
+export default defineConfig({
+  resolve: {
+    alias: genAlias(["constant", "utils", "components", "pages", "app", "types", "presenters"]),
+  },
+});
